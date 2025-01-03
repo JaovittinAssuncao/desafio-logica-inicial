2a5f747d06ae338785002e396f6f0c2ebb0f5a92
JaovittinAssuncao <joaovitorassuncao2010@gmail.com>
2025-01-02 22:35:32 -0300

    meu segundo projeto para desafio logico

---
+++ src/index.js
@@ -0,0 +1,47 @@
+let nomeHeroi = "joaovitao"
+let xpHeroi = 61
+let patenteHeroi = ""
+
+if(xpHeroi <= 1000){
+      patenteHeroi = "Ferro"
+      console.log("O Herói de nome " + nomeHeroi + " está no nível de " + patenteHeroi)
+}
+
+if(xpHeroi >= 1001 && (xpHeroi <= 2000) ) {
+    patenteHeroi = "Bronze"
+      console.log("O Herói de nome " + nomeHeroi + " está no nível de " + patenteHeroi)
+}
+if(xpHeroi >= 2001 && (xpHeroi <= 5000) ) {
+    patenteHeroi = "Prata"
+      console.log("O Herói de nome " + nomeHeroi + " está no nível de " + patenteHeroi)
+}
+if(xpHeroi >= 5001 && (xpHeroi <= 7000) ) {
+    patenteHeroi = "Ouro"
+      console.log("O Herói de nome " + nomeHeroi + " está no nível de " + patenteHeroi)
+}
+if(xpHeroi >= 7001 && (xpHeroi <= 8000) ) {
+    patenteHeroi = "Platina"
+      console.log("O Herói de nome " + nomeHeroi + " está no nível de " + patenteHeroi)
+}
+if(xpHeroi >= 8001 && (xpHeroi <= 9000) ) {
+    patenteHeroi = "Ascendente"
+      console.log("O Herói de nome " + nomeHeroi + " está no nível de " + patenteHeroi)
+}
+if(xpHeroi >= 9001 && (xpHeroi <= 10000) ) {
+    patenteHeroi = "Imortal"
+      console.log("O Herói de nome " + nomeHeroi + " está no nível de " + patenteHeroi)
+}
+if(xpHeroi >= 10001 ) {
+    patenteHeroi = "Radiante"
+      console.log("O Herói de nome " + nomeHeroi + " está no nível de " + patenteHeroi)
+}
+
+
+
+ 
+
+ 
+
+
+
+ 
